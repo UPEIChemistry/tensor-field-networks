1da1f2a03ee0868ee1694450b2819f454e78e774
Riley Jackson <rjjackson22@gmail.com>
2019-08-01 13:38:05 -0300

    Updated setup.py script

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='tfn-Layers',
+    name='tfn-layers',
     author='Riley Jackson',
     author_email='rjjackson@upei.ca',
-    version='0.0.0',
+    version='1.0.0',
     description='Keras layers for rotationally equivariant Convolutional Tensor Field Networks',
     packages=find_packages(),
-    install_requires=['tensorflow==2.0.0-beta1', 'numpy']
+    install_requires=['tensorflow_gpu==2.0.0-beta1', 'numpy'],
+    extras_require={
+        'tests': ['pytest']
+    }
 )
