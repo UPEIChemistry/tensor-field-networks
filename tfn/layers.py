fe0d71dce0b656b07ec15f8cb5d6c3d0f624da8d
Riley Jackson <rjjackson22@gmail.com>
2019-09-04 16:03:51 -0300

    Closes #6

--- tfn/layers.py
+++ tfn/layers.py
@@ -10,13 +10,28 @@ from tensorflow.python.keras.layers import Dense, Layer
 
 import tfn.wrappers
 from tfn import utils
+import json
 
 
 class RadialFactory(object):
     """
     Default factory class for supplying radial functions to a Convolution layer. Subclass this factory and override its
-    'get_radial' method to return custom radial instances/templates.
+    `get_radial` method to return custom radial instances/templates. You must also override the `to_json` and
+    `from_json` and register any custom `RadialFactory` classes to a unique string in the keras global custom objects
+    dict.
     """
+    def __init__(self,
+                 num_layers: int = 2,
+                 units: int = 16,
+                 kernel_lambda: float = 0.,
+                 bias_lambda: float = 0.,
+                 dynamic: bool = False):
+        self.num_layers = num_layers
+        self.units = units
+        self.kernel_lambda = kernel_lambda
+        self.bias_lambda = bias_lambda
+        self.dynamic = dynamic
+
     def get_radial(self, feature_dim, input_order=None, filter_order=None):
         """
         Factory method for obtaining radial functions of a specified architecture, or an instance of a radial function
@@ -24,23 +39,34 @@ class RadialFactory(object):
 
         :param feature_dim: Dimension of the feature tensor being point convolved with the filter produced by this
             radial function. Use to ensure radial function outputs a filter of shape (points, feature_dim, filter_order)
-        :param input_order: Optional. Rotation order of the of the feature tensor point convolved with the filter produced
-            by this radial function
+        :param input_order: Optional. Rotation order of the of the feature tensor point convolved with the filter
+            produced by this radial function
         :param filter_order: Optional. Rotation order of the filter being produced by this radial function.
         :return: Keras Layer object, or subclass of Layer. Must have attr dynamic == True and trainable == True.
         """
         return Sequential([
             Dense(
-                32,
-                kernel_regularizer=regularizers.l2(0.01),
-                bias_regularizer=regularizers.l2(0.01)
-            ),
+                self.units,
+                kernel_regularizer=regularizers.l2(self.kernel_lambda),
+                bias_initializer=regularizers.l2(self.bias_lambda),
+                dynamic=self.dynamic
+            )
+            for _ in range(self.num_layers)
+        ] + [
             Dense(
                 feature_dim,
-                kernel_regularizer=regularizers.l2(0.01),
-                bias_regularizer=regularizers.l2(0.01)
+                kernel_regularizer=regularizers.l2(self.kernel_lambda),
+                bias_initializer=regularizers.l2(self.bias_lambda),
+                dynamic=self.dynamic
             )
         ])
+
+    def to_json(self):
+        return json.dumps(self.__dict__)
+
+    @classmethod
+    def from_json(cls, config):
+        return cls(**config)
 
 
 class Convolution(Layer):
