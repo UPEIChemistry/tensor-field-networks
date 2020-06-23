2fc8fba38a52b9f4dea748e5987c8614ed755e6c
Riley Jackson <rjjackson22@gmail.com>
2020-06-23 15:55:00 -0300

    Modified qm9/0 to contain deeper residual network

--- runs/qm9/0-defaults/exp_qm9_0.py
+++ runs/qm9/0-defaults/exp_qm9_0.py
@@ -2,6 +2,7 @@ from tfn.tools.jobs import SingleModel
 
 job = SingleModel(exp_config={
     'name': 'QM9 E PREDICTION USING DEFAULT ENERGY MODEL',
-    'notes': 'Using all defaults'
+    'notes': 'Using all defaults',
+    'builder_config': {'num_layers': (4, 3, 2), 'si_units': (32, 16, 8)}
 })
 job.run()
