3ae6582ff515ec4b09b7f82cc4ababef6d196abc
Riley Jackson <rjjackson22@gmail.com>
2020-06-24 11:34:33 -0300

    WIP: UNTESTED Added ability to request different types of formats for TS data

--- tfn/tools/loaders/ts_loader.py
+++ tfn/tools/loaders/ts_loader.py
@@ -45,18 +45,27 @@ class TSLoader(DataLoader):
         reactant/ts/product cartesians & atomic numbers obtained using the MP2 method with the
         cc-PVDZ basis set.
 
-        :return: data in the format:
-        [
-            (x_train, y_train),
-            (x_val, y_val),
-            (x_test, y_test)
-        ]
-        Where x is: [atomic_numbers, reactant_cartesians, reactant_complex_cartesians,
-        product_cartesians, product_complex_cartesians]
-        Where y is: [ts_cartesians]
+        x: [atomic_numbers, reactant_cartesians, reactant_complex_cartesians,
+            product_cartesians, product_complex_cartesians] if kwarg `input_type` ==
+            'cartesians' (Default). If kwarg `input_type` == 'energies', then x is: [
+            atomic_numbers, reactant_energies, product_energies]. Very rarely will this be
+            useful, but the functionality exists.
+        y: [ts_cartesians] if kwarg `output_type` == 'cartesians' (Default). If kwarg
+        'output_type' == 'energies' then y is: [ts_energies], and if `output_type` == 'both'
+        then y is: [ts_cartesians, ts_energies]
+
+        :param kwargs: Possible kwargs:
+            'cache': bool. Defaults to True.
+            'input_type': str. Defaults to 'cartesians'. Possible values include ['cartesians',
+                'energies', 'classifier'],
+            'output_type': str. Defaults to 'cartesians'. Possible values include ['cartesians',
+            'energies', 'both', 'classifier']
+        :return: data in the format: [(x_train, y_train), (x_val, y_val), (x_test, y_test)]
         """
-        if self._data is not None:
+        if self._data is not None and kwargs.get('cache', True):
             return self._data
+
+        # Load Data
         with File(self.path, 'r') as dataset:
             atomic_nums = self.pad_along_axis(
                 np.asarray(dataset['ts/atomic_numbers'], dtype='int'),
@@ -68,44 +77,71 @@ class TSLoader(DataLoader):
                 ) for structure_type in
                 ('reactant', 'reactant_complex', 'ts', 'product_complex', 'product')
             }
+            energies = {
+                structure_type: self.pad_along_axis(
+                    np.asarray(dataset['{}/energies'.format(structure_type)]),
+                    self.num_atoms
+                ) for structure_type in
+                ('reactant', 'ts', 'product')
+            }
 
+        # Remap
         if self.map_atoms:
             self.remap_atoms(atomic_nums)
         self._max_z = np.max(atomic_nums) + 1
         if kwargs.get('return_maxz', False):
             return
 
-        x = [
+        # Set up possible inputs/outputs
+        x, y = [], []
+        x_energies = [
+            energies['reactant'],
+            energies['product']
+        ]
+        x_cartesians = [
             atomic_nums,
             cartesians['reactant'],
             cartesians['reactant_complex'],
             cartesians['product'],
             cartesians['product_complex']
         ]
-        y = [
+        y_energies = [
+            energies['ts']
+        ]
+        y_cartesians = [
             cartesians['ts']
         ]
+
+        # Select requested inputs/outputs
+        input_type = kwargs.get('input_type', 'cartesians').lower()
+        output_type = kwargs.get('output_type', 'cartesians').lower()
+        if input_type == 'classifier' or output_type == 'classifier':
+            atomic_nums = np.tile(atomic_nums, (5, 1))
+            cartesians = np.concatenate(
+                [a for a in cartesians.values()],
+                axis=0)
+            labels = np.zeros((5 * len(atomic_nums),))
+            labels[2 * len(atomic_nums): 3 * len(atomic_nums) + 1] = 1
+            x = [atomic_nums, cartesians]
+            y = [labels]
+        elif input_type == 'energies':
+            x.extend(x_energies)
+        elif input_type == 'both':
+            x.extend(x_cartesians)
+            x.extend(x_energies)
+        else:
+            x.extend(x_cartesians)
+        if output_type == 'energies':
+            y.extend(y_energies)
+        elif output_type == 'both':
+            y.extend(y_cartesians)
+            y.extend(y_energies)
+        else:
+            y.extend(y_cartesians)
+
+        # Split and serve data
         self._data = self.split_dataset(
             data=[x, y],
             length=len(atomic_nums)
         )
         return self._data
-
-    def load_distance_data(self, *args, **kwargs):
-        """DEPRICATED"""
-        with File(self.path, 'r') as dataset:
-            x = np.array(dataset['equilibria_distances'])
-            y = np.array(dataset['ts_distances'])
-
-        x = np.concatenate(
-            [x, np.concatenate([x[:, :2], x[:, 3:4], x[:, 2:3], x[:, 5:], x[:, 4:5]], axis=-1)]
-        )
-        y = np.concatenate(
-            [y, np.concatenate([y[:, 1:], y[:, :1]], axis=-1)]
-        )
-
-        if kwargs.get('split', False):
-            (x_train, y_train), (x_test, y_test) = self.split_dataset([[x], [y]], length=len(x))
-            return (x_train[0], y_train[0]), (x_test[0], y_test[0])
-        else:
-            return x, y
