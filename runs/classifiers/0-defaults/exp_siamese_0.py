cbecb335de789ebc58b6ab7ad85ef3b570ebe503
Riley Jackson <rjjackson22@gmail.com>
2020-07-03 19:05:57 -0300

    Changed defaults and updated exp_siamese_0.py

--- runs/classifiers/0-defaults/exp_siamese_0.py
+++ runs/classifiers/0-defaults/exp_siamese_0.py
@@ -1,10 +1,16 @@
 from tfn.tools.jobs import SingleModel
+import os
+
+os.environ['DATADIR'] = '/home/riley/dev/python/data'
 
 job = SingleModel(exp_config={
     'name': 'SIAMESE CLASSIFIER ON TS DATASET EXPERIMENT 0',
     'notes': 'Using all defaults',
     'run_config': {
-        'fit_verbosity': 1
+        'fit_verbosity': 1,
+        'class_weights': {0: 1, 1: 5},
+        'loss': 'binary_crossentropy',
+        'metrics': ['accuracy']
     },
     'loader_config': {
         'loader_type': 'ts_loader',
@@ -12,8 +18,14 @@ job = SingleModel(exp_config={
     },
     'builder_config': {
         'builder_type': 'siamese_builder',
-        'num_layers': (4, 3, 2),
-        'si_units': (32, 16, 8)
+        'num_layers': (4, 4, 4),
+        'si_units': (32, 32, 16),
+        'basis_config': {
+            'width': 0.2,
+            'spacing': 0.05,
+            'min_value': -1.0,
+            'max_value': 10.0
+        }
     }
 })
 job.run()
