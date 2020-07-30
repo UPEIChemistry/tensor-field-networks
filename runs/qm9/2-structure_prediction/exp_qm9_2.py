47dcebf6bb7fe4e195785a501212669b89ac19cc
Riley Jackson <rjjackson22@gmail.com>
2020-07-30 17:06:54 -0300

    updated qm9_exp_2.py

--- runs/qm9/2-structure_prediction/exp_qm9_2.py
+++ runs/qm9/2-structure_prediction/exp_qm9_2.py
@@ -4,6 +4,7 @@ job = SingleModel(exp_config={
     'name': 'QM9 E PREDICTION USING DEFAULT ENERGY MODEL',
     'notes': 'Using all defaults',
     'loader_config': {'loader_type': 'qm9_loader', 'load_kwargs': {'modify_structures': True}},
-    'builder_config': {'builder_type': 'ts_builder'}
+    'builder_config': {'builder_type': 'ts_builder', 'num_layers': (2, 2, 2),
+                       'si_units': (64, 32, 16)}
 })
 job.run()
