7c912b0ef0af75748c64e0ee7984fda577622efc
Riley Jackson <rjjackson22@gmail.com>
2020-08-04 11:42:38 -0300

    Fixed learning rate in exp_ts_0.py

--- runs/ts/0-defaults/exp_ts_0.py
+++ runs/ts/0-defaults/exp_ts_0.py
@@ -4,7 +4,7 @@ job = SingleModel(exp_config={
     'name': 'DEFAULT TS MODEL ON TS DATASET',
     'notes': 'Train on only distance matrix',
     'run_config': {'epochs': 200, 'loss': 'mae', 'optimizer_kwargs': {
-        'learning_rate': 0.1}},
+        'learning_rate': 0.001}},
     'builder_config': {'builder_type': 'ts_builder', 'num_layers': (2, 2, 2),
                        'si_units': 64},
     'loader_config': {
