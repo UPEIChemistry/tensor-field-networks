940595a31cf202afcb45e046778d9dee2021e264
Riley Jackson <rjjackson22@gmail.com>
2020-08-04 11:48:48 -0300

    Update number of epochs to run for

--- runs/ts/0-defaults/exp_ts_0.py
+++ runs/ts/0-defaults/exp_ts_0.py
@@ -3,7 +3,7 @@ from tfn.tools.jobs import SingleModel
 job = SingleModel(exp_config={
     'name': 'DEFAULT TS MODEL ON TS DATASET',
     'notes': 'Train on only distance matrix',
-    'run_config': {'epochs': 200, 'loss': 'mae', 'optimizer_kwargs': {
+    'run_config': {'epochs': 500, 'loss': 'mae', 'optimizer_kwargs': {
         'learning_rate': 0.001}},
     'builder_config': {'builder_type': 'ts_builder', 'num_layers': (2, 2, 2),
                        'si_units': 64},
