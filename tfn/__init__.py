54916299cfa74c8ab15ba758b5c758c17baefa2c
Riley Jackson <rjjackson22@gmail.com>
2020-08-13 13:44:00 -0300

    v2.0.0 for tfn package

--- tfn/__init__.py
+++ tfn/__init__.py
@@ -2,7 +2,9 @@
 Package containing Tensorfield Network tf.keras layers built using TF 2.0
 """
 
-__version__ = '1.3.0'
+__version__ = "2.0.0"
 __author__ = "Riley Jackson"
-__email__ = 'rjjackson@upei.ca'
-__description__ = 'Package containing Tensor Field Network tf.keras layers built using TF 2.0'
+__email__ = "rjjackson@upei.ca"
+__description__ = (
+    "Package containing Tensor Field Network tf.keras layers built using Tensorflow 2"
+)
