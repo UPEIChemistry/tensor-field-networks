42fbe84ad49b8c29603c279a0b5839144a0626d3
Riley Jackson <rjjackson22@gmail.com>
2020-08-26 15:03:40 -0300

    Updated experiments and incremented version number to 2.2.0

--- runs/pipeline/qm9_to_ts/cartesian_to_cartesian/experiment.py
+++ runs/pipeline/qm9_to_ts/cartesian_to_cartesian/experiment.py
@@ -1,34 +1,44 @@
 from pathlib import Path
-from tfn.tools.jobs import Pipeline
+from tfn.tools.jobs import Pipeline, StructurePrediction, CrossValidate
 
 
 job = Pipeline(
-    {
-        "name": f"{Path(__file__).parent}",
-        "notes": "",
-        "pipeline_config": {
-            "configs": [
-                {  # QM9 config
-                    "builder_config": {
-                        "builder_type": "cartesian_builder",
-                        "prediction_type": "cartesians",
-                        "output_types": "cartesians",
-                    },
-                    "loader_config": {
-                        "loader_type": "qm9_loader",
-                        "load_kwargs": {"modify_structures": True},
-                    },
+    exp_config={"name": f"{Path(__file__).parent}", "seed": 608638837,},
+    jobs=[
+        StructurePrediction(
+            exp_config={
+                "name": f"{Path(__file__).parent}",
+                "notes": "",
+                "loader_config": {
+                    "loader_type": "qm9_loader",
+                    "load_kwargs": {"modify_structures": True},
                 },
-                {  # TS config
-                    "builder_config": {
-                        "builder_type": "cartesian_builder",
-                        "prediction_type": "cartesians",
-                        "output_types": "cartesians",
-                    },
-                    "loader_config": {"loader_type": "ts_loader"},
+                "builder_config": {
+                    "builder_type": "cartesian_builder",
+                    "prediction_type": "cartesians",
+                    "output_type": "cartesians",
                 },
-            ]
-        },
-    }
+            }
+        ),
+        CrossValidate(
+            exp_config={
+                "name": f"{Path(__file__).parent}",
+                "notes": "",
+                "seed": 608638837,
+                "run_config": {
+                    "epochs": 500,
+                    "loss": "mae",
+                    "metrics": ["cumulative_loss"],
+                },
+                "loader_config": {"loader_type": "ts_loader"},
+                "builder_config": {
+                    "builder_type": "cartesian_builder",
+                    "prediction_type": "cartesians",
+                    "output_type": "cartesians",
+                },
+                "lr_config": {"min_delta": 0.01, "patience": 30, "cooldown": 20},
+            }
+        ),
+    ],
 )
 job.run()
